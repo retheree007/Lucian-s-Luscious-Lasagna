cb94d55c965db5a4af0a86c5fb8cf65a5adad9e9
rethr <rethr@DESKTOP-BB24RP2>
2023-03-29 16:59:36 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Code_Coach_Challenge
+{
+
+    class Lassgna
+    {
+        private int expected = 40;
+
+        public int ExpectedMinutesInOven()
+        {
+            return expected ;
+        }
+
+        public int RemainingMinutesInOven(int timeinoven)
+        {
+            return ExpectedMinutesInOven() - timeinoven;
+        }
+
+        public int PreparationTimeInMinutes(int layers)
+        {
+            return layers * 2;
+        }
+
+        public int ElapsedTimeInMinutes(int timeinoven, int layers)
+        {
+            return timeinoven + layers * 2;
+        }
+    }
+
+}
